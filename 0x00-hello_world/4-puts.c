cf476a182f3ee5a7c11b0d5ccc81bad6350c6daa
cymaxy <okorocyril32@gmail.com>
2022-06-30 13:57:19 -0700

    My commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,11 @@
 /**
  *main - entry point
  *
- *return: 0 after printing the function
+ *return: 0 after printing the funtion
+ *Return: 0 after printing the funtion
  */
 int main(void)
-puts("\"programming is like building a multilingual puzzle ");
+{
+puts("\"programming is like building a multilingual puzzle");
 return (0) ;
 }

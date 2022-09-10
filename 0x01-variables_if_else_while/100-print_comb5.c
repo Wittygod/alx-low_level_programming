0ef1b3e7d04e9fa7a4a7fde8d159047e6c943f67
Wittygod <cyrilodinaka32@gmail.com>
2022-09-10 10:49:32 -0700

    12th commit

--- 0x01-variables_if_else_while/100-print_comb5.c
+++ 0x01-variables_if_else_while/100-print_comb5.c
@@ -1,50 +1,44 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - main block
- * Description: Print all possible combinations of two two-digit numbers.
- * Numbers should range from 0 to 99.
- * The two numbers should be separated by a space.
- * All numbers should be printed with two digits. 1 should be printed as 01.
- * Combination of numbers must be separated by a comma followed by a space.
- * Combinations of numbers should be printed in ascending order.
- * `00 01` and `01 00` are considered as the same combination.
- * You can only use `putchar` to print to console.
- * You can only use `putchar` up to 8 times.
- * You are not allowed to use any variable of type `char`.
- * Return: 0
+ * main - Entry point
+ * Description: prints all possible combination of 2-digit numbers
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int i, j;
-	int a, b, c, d;
+	int c, i, k, j;
 
-	for (i = 0; i < 100; i++)
+	for (c = 48; c <= 57; c++)
 	{
-		a = i / 10; /* doubles fnum */
-		b = i % 10; /* singles fnum */
-
-		for (j = 0; j < 100; j++)
+		for (i = 48; i <= 57; i++)
 		{
-			c = j / 10; /* doubles snum */
-			d = j % 10; /* singles snum */
-
-			if (a < c || (a == c && b < d))
+			for (k = 48; k <= 57; k++)
 			{
-				putchar(a + '0');
-				putchar(b + '0');
-				putchar(32);
-				putchar(c + '0');
-				putchar(d + '0');
-
-				if (!(a == 9 && b == 8))
+				for (j = 48; j <= 57; j++)
 				{
-					putchar(44);
-					putchar(32);
+				if (((k + j) > (c + i) &&  k >= c) || c < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(' ');
+					putchar(k);
+					putchar(j);
+
+					if (c + i + k + j == 227 && c == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
 				}
 			}
 		}
 	}
-	putchar(10);
-
+	putchar('\n');
 	return (0);
 }

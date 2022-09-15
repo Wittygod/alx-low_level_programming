01050d1567c3cde2a01659d13e4e6d4fdd1b7813
Wittygod <cyrilodinaka32@gmail.com>
2022-09-15 12:59:22 -0700

    hard things

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - to check for upper case
+ * @c: character input variable
+ *
+ * Return: 1 for positive, 0 for negative
+ */
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
